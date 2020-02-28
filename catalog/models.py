d7356f65d4c80e65d43fe191a79b8cf4e9251381
mathewb <mathew.bryan@radiuspaymentsolutions.com>
2020-02-28 14:09:06 +0000

    added site login

--- catalog/models.py
+++ catalog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
 # Used to generate URLs by reversing the URL patterns
 from django.urls import reverse
 import uuid
+
 
 # Create your models here.
 
@@ -44,10 +47,13 @@ class Book(models.Model):
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
-    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True) 
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book across whole library')
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -70,6 +76,13 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
 
 class Author(models.Model):
     """Model representing an author."""
